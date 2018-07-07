77266dfc0bafdb2f74e069a12421330bd8e31295
DirectX10 <ak9734t10@gmail.com>
2018-07-07 13:30:32 +0300

    HelloWorld

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Grid from 'react-bootstrap/lib/Grid';
+import Row from 'react-bootstrap/lib/Row';
+import Col from 'react-bootstrap/lib/Col';
+import styled from 'styled-components';
+
+function Hello(props){
+  return (
+    <h1> Hello {props.name}! </h1>
+  )
+}
+
+class App extends Component {
+  render() {
+    return (
+      <Hello name="World" />
+    );
+  }
+}
+
+export default App;
